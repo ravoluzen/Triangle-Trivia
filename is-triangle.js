7ba7abcded60393975a73fcf3c04754a5452b952
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2021-10-03 14:50:42 +0530

    Restructured files, minor fixes and changes

--- is-triangle.js
+++ is-triangle.js
@@ -6,11 +6,18 @@ const result = document.querySelector("#result");
 
 function triangleCheck(){
     console.log("working");
-    if((firstAngle.value + secondAngle.value + thirdAngle.value) === 180){
+    angleOne = Number(firstAngle.value);
+    angleTwo = Number(secondAngle.value);
+    angleThree = Number(thirdAngle.value);
+
+    const angleSum = angleOne + angleTwo + angleThree;
+
+    if(angleSum == 180){
         result.innerText = "Yes! These angles can form a triangle! :)";
     }else{
         result.innerText = "Oops! These angles can't form a triangle. :(";
     }
+    console.log(angleSum);
 }
 
 checkButton.addEventListener("click", triangleCheck);
